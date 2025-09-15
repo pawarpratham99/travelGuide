6c2df8b2117de5b551cc5d25f04e973a1e2fadf9
Pawar Pratham 99 <skillboost999@gmail.com>
2025-09-15 10:44:08 +0530

    home page and popular places

--- app/src/main/java/com/example/travelapp/list_item.kt
+++ app/src/main/java/com/example/travelapp/list_item.kt
@@ -4,11 +4,19 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import android.content.Intent
+import android.widget.TextView
+
 
 class list_item : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activityitem) // ✅ this has your RecyclerView
+        val seeAllTextView = findViewById<TextView>(R.id.tv_see_all_popular)
+        seeAllTextView.setOnClickListener {
+            val intent = Intent(this, popular_places::class.java)
+            startActivity(intent)
+        }
 
         // Find RecyclerView
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
@@ -20,9 +28,23 @@ class list_item : AppCompatActivity() {
         val destinations = listOf(
             Destination(R.drawable.nilhadri, "Nilhadri", "Odisha","4.8"),
             Destination(R.drawable.tajmahal, "Taj Mahal", "Agra","4.6") ,
-            Destination(R.drawable.tajmahal, "Taj Mahal", "Agra","4.6")
+            Destination(R.drawable.qutubminar, "Qutub Minar", "Delhi","4.6") ,
+            Destination(R.drawable.redfort, "Red Fort", "Delhi","4.6"),
+            Destination(R.drawable.goldentemple, "Golden temple", "Delhi","4.6")
+
+        )
+        val recyclerViewLarge = findViewById<RecyclerView>(R.id.rv_popular_destinations)
+        recyclerViewLarge.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+
+        val largeDestinations = listOf(
+            Destination(R.drawable.qutubminar, "Qutub Minar", "Delhi","4.6"),
+            Destination(R.drawable.redfort, "Red Fort", "Delhi","4.6"),
+            Destination(R.drawable.goldentemple, "Golden Temple", "Amritsar","4.7")
         )
 
+        recyclerViewLarge.adapter = DestinationAdapter(
+            largeDestinations)
         // Attach adapter
         recyclerView.adapter = DestinationAdapter(destinations)
     }
--- app/src/main/java/com/example/travelapp/popular_places.kt
+++ app/src/main/java/com/example/travelapp/popular_places.kt
@@ -5,16 +5,33 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+
+import androidx.recyclerview.widget.RecyclerView
 
 class popular_places : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_popular_places)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        // ---------------- New RecyclerView (recyclerViewDestinations) ----------------
+        val recyclerViewDestinations = findViewById<RecyclerView>(R.id.recyclerViewDestinations)
+        // ✅ Use GridLayoutManager → 2 columns (2 images per row)
+        recyclerViewDestinations.layoutManager = GridLayoutManager(this, 2)
+
+        val extraDestinations = listOf(
+            Destination(R.drawable.nilhadri, "Nilhadri", "Odisha","4.8"),
+            Destination(R.drawable.tajmahal, "Taj Mahal", "Agra","4.6"),
+            Destination(R.drawable.redfort, "Red Fort", "Delhi","4.5"),
+            Destination(R.drawable.redfort, "Red Fort", "Delhi","4.5"),
+            Destination(R.drawable.tajmahal, "Taj Mahal", "Agra","4.6"),
+            Destination(R.drawable.tajmahal, "Taj Mahal", "Agra","4.6"),
+
+            )
+        recyclerViewDestinations.adapter = DestinationAdapter(
+            extraDestinations
+         )
     }
-}
+
+        }
